c16e707235d4a657db93068a9e84d1627e8be334
Sebas <sheva3232@gmail.com>
2023-11-23 07:56:58 -0500

    adjusting project files_

--- src/components/UseToken.ts
+++
@@ -1,7 +0,0 @@
-export interface UseTokenProps {
-  token?: string;
-}
-export declare function useToken(props?: UseTokenProps): {
-  getTokenProps: (props?: any) => any;
-};
-export declare type UseDisclosureReturn = ReturnType<typeof useToken>;
